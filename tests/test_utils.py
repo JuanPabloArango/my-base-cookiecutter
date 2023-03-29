03533345eaf00b9e634f9f7f5527248dd31374b2
JuanPabloArango <juanpabloarangosa@gmail.com>
2023-03-29 13:32:57 -0500

    Ruta de test unitarios

---
+++ tests/test_utils.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import pytest
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+from src.utils import fibonacci
+
+def test_fibonacci():
+
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+
+    print("YAAY")
+
+    
+if __name__ == "__main__":
+    test_fibonacci()
